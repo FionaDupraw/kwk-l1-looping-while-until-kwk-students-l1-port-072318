37e45a843d7eb90622823e61071db0c2bc2beabc
FionaDupraw <fionadupraw@gmail.com>
2018-07-24 22:29:18 +0000

    automatically backed up by learn

--- hogwarts.rb
+++ hogwarts.rb
@@ -1,7 +1,7 @@
 puts "Wingardium Leviosa"
 feather = 6
 while feather < 10
-puts "Wingardium Leviosa"
-puts "#{feather + 1}"
-feathers += 1
+  puts "Wingardium Leviosa"
+  puts "#{feather + 1}"
+  feathers += 1
 end
